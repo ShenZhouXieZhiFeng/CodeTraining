3c4e4c2e15a7ebbf7c086a7ebf3a5b5812dc64be
Stone <xiezf001@qq.com>
2019-01-25 16:58:12 +0800

    unity commit

---
+++ Unity/CodeTraining/Assets/Scripts/Move1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("updatePos", 0, 0.05f);
+    }
+
+    void updatePos()
+    {
+        float dt = (float)Time.time;
+        float posX = Mathf.Sin(dt);
+        float posY = Mathf.Cos(dt);
+        this.transform.position = new Vector3(posX,posY,0);
+    }
+}
